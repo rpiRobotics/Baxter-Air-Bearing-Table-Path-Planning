965fd559f890a823b1be46963bdda98c26e5722c
dcarabis <dcarabis@users.noreply.github.com>
2020-09-23 16:13:25 -0400

    Add files via upload

---
+++ equality_constraints_rotation.m
@@ -0,0 +1,19 @@
+function [Aeq,beq] = equality_constraints_rotation(RT,Rc,k,J)
+
+R = RT*Rc;
+
+RPY = tr2rpy(R);
+thetax = RPY(1); thetay = RPY(2); thetaz = RPY(3);
+
+thetadot = thetadot_zrot(thetax,thetay,thetaz,k);
+
+
+wc = [[1;0;0],rotx(thetax)*[0;1;0], ...
+    rotx(thetax)*roty(thetay)*[0;0;1]]*thetadot;
+
+
+% for rotation about z axis:
+beq = [wc(1);wc(2)];
+Aeq = [J(1:2,:),zeros(2,2)];
+
+end
